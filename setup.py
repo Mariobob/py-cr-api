b2d75c59aaf960b3823008bbabc4c9f138c422fd
freetnt5852 <31079629+freetnt5852@users.noreply.github.com>
2017-12-26 18:11:58 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='py-cr-api',
+    version='1.0.0',
+    url='https://github.com/freetnt5852/py-cr-api',
+    license='MIT',
+    author='Free TNT',
+    description='a python api wrapper for cr-api.com to learn',
+    packages=['py-cr-api']
+    )
+    
+    
