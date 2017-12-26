e263ce14b555d0013d4db3166d4896959c4b0926
freetnt5852 <31079629+freetnt5852@users.noreply.github.com>
2017-12-26 18:20:43 +0300

    Ops

--- py_cr_api/__init__.py
+++ py_cr_api/__init__.py
@@ -4,4 +4,4 @@ __title__ = "PY CR-API"
 __version__ = "1.0.0"
 __license__ = "MIT"
 __author__ = "Free TNT"
-__github__ = "https://github.com/freetnt5852/py-cr-api
+__github__ = "https://github.com/freetnt5852/py-cr-api"
